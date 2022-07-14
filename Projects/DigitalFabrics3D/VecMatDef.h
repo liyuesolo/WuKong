574d632e14a445d7f24bf29f1e784afd767d4ae6
liyuesolo <yueli.cg@gmail.com>
2022-07-14 16:33:09 +0200

    clean up

--- Projects/DigitalFabrics3D/VecMatDef.h
+++
@@ -1,7 +0,0 @@
-template <typename T, int dim>
-using Vector = Eigen::Matrix<T, dim, 1, 0, dim, 1>;
-
-template <typename T, int n, int m>
-using Matrix = Eigen::Matrix<T, n, m, 0, n, m>;
-
-
--- Projects/TopographyOptimization/tests.cpp
+++
@@ -1,2 +0,0 @@
-#include "FEMSolver.h"
-
